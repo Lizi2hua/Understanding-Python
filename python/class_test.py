b9d058d5b46dd44d993926a230dcdca268550eec
Natuski <1050100964@qq.com>
2020-05-08 19:35:53 +0800

    modified class_test.py

---
+++ python/class_test.py
@@ -0,0 +1,8 @@
+
+#https://www.yiibai.com/python/python_classes_objects.html
+#self用来代替将会实例化的类的类本身（self.name-->student.name)
+class Employee:
+    "commom base class for all employee"
+    empCount=0
+
+    def
