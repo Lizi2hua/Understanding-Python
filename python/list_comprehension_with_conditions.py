3dfad35a05d744a54345c5781c693060b31ce221
Natuski <1050100964@qq.com>
2020-05-08 18:58:41 +0800

    add introduction to class and list comprehesion with conditions

---
+++ python/list_comprehension_with_conditions.py
@@ -0,0 +1,13 @@
+num1=[5,10,15]
+#列表推导式
+num2=[i**2 if i==10 else i-5 if i<7 else i+5 for i in num1 ]
+print(num2)
+#执行顺序,if的执行语句是左边的语句
+for i in num1:
+    if i==10:
+        i=i**2
+    else:
+        if i<7:
+            i=i-5
+        else:i=i+5
+    print(i)
