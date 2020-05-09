fd1e694a8b15dd973d32ae7c2918ebf977025512
LiZi Hua <1050100964@qq.com>
2020-05-09 22:34:34 +0800

    add value_count method

--- python/data_cleaning.py
+++ python/data_cleaning.py
@@ -8,11 +8,18 @@ Created on Sat May  9 17:15:30 2020
 import pandas as pd
 
 #*************read data**********#
-data=pd.read_csv('C:\\Users\\Administrator\\Desktop\\Project：777\\data\\pokemon-challenge\\pokemon.csv')
+#data=pd.read_csv('C:\\Users\\Administrator\\Desktop\\Project：777\\data\\pokemon-challenge\\pokemon.csv')
+data=pd.read_csv('C:\\Users\\李梓桦\\Desktop\\培训V20200507\\dataset\\pokemon-challenge\\pokemon.csv')
 datahead=data.head() #show first 5 rows
 print(datahead)
 datatial=data.tail()
 print(datatial)#show last 5 rows
 datacolumn=data.columns
 print(datacolumn)
+print(data.shape)
+print("-------------------")
+print(data.info)
+#look frequency of pokemon types
+print(data['Type 1'].value_counts(dropna=False))
+#value_count（）需要指定对哪一列使用，这里是data中的Type 1
 #*******************************#
